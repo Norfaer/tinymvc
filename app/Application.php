f0b703be08743381bbd6188ad9bb45ba643ad210
root <root@norfaer.norfaer.com>
2015-10-27 16:20:53 +0200

    Localy commited

---
+++ app/Application.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tiny\Application;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once __DIR__."/Http.php";
+
+class Application
+{
+    public function __construct() {
+        
+    }
+    public function init() {
+        
+    }
+    public function run() {
+        echo "Hello";
+    }
+}
---
+++ app/Http.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tuny\HttpBase;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Request {
+    public $attributes;
+    public $query;
+    public $cookies;
+    public function __construct() {
+        ;
+    }
+}
+
+class Response {
+    
+}
